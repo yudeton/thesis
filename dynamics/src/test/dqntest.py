741d752c7162350cb96fcb746c2683cbec0d144a
yudeton <yudeton@gmail.com>
2024-03-04 14:43:11 +0100

    03042143

---
+++ dynamics/src/test/dqntest.py
@@ -0,0 +1,47 @@
+import math, random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd 
+import torch.nn.functional as F
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+USE_CUDA = torch.cuda.is_available()
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
+from collections import deque
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state      = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
+env_id = "CartPole-v0"
+env = gym.make(env_id)
+
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 500
+
+epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+plt.plot([epsilon_by_frame(i) for i in range(10000)])
+plt.show()
